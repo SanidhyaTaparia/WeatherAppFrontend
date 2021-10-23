154931810a38f8abcd0f37d6380fb5477e5a68b9
SanidhyaTaparia <tapariasanidhya@gmail.com>
2021-10-23 14:21:13 +0530

    Added WeatherStation and AirQuality Functions

--- src/App.js
+++ src/App.js
@@ -8,60 +8,104 @@ import {
 } from 'react-router-dom';
 import CurrentWeather from './components/CurrentWeather/CurrentWeather';
 import Forecast from './components/Forecast/Forecast';
+import AirQuality from './components/AirQuality/AirQuality';
+import WeatherStation from './components/WeatherStation/WeatherStation';
 import axios from "axios";
 
 
 function App() {
   const [weather,setWeather]=useState([])
   const [forecast,setForecast]=useState([])
+  const [airquality,setAirquality]=useState([])
+  const [station,setStation]=useState([])
 
-  useEffect(()=>{
-    console.log("useEffect called");
-    async function fetchData(){
-        const options = {
-                method: 'GET',
-                url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-                params: {
-                    q: 'London,uk',
-                    lat: '0',
-                    lon: '0',
-                    // callback: 'test',
-                    id: '2172797',
-                    lang: 'null',
-                    units: 'imperial',
-                    mode: 'json'
-                },
-                headers: {
-                    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-                    'x-rapidapi-key': '47cd745e3amshab32e2942dd9b87p1e6fa4jsn14c37e1f98f1'
-                }
-            };
-        const request=await axios.request(options)
-        console.log("Weather Request=",request.data);
-        setWeather(request.data)
-        // return request
-    }
-    fetchData();
-},[])
+//   useEffect(()=>{
+//     console.log("useEffect called");
+//     async function fetchData(){
+//         const options = {
+//                 method: 'GET',
+//                 url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+//                 params: {
+//                     q: 'London,uk',
+//                     lat: '0',
+//                     lon: '0',
+//                     // callback: 'test',
+//                     id: '2172797',
+//                     lang: 'null',
+//                     units: 'imperial',
+//                     mode: 'json'
+//                 },
+//                 headers: {
+//                     'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+//                     'x-rapidapi-key': '92892ab54amshf55ba9722bf8547p1eedb8jsn24eaeb644984'
+//                 }
+//             };
+//         const request=await axios.request(options)
+//         console.log("Weather Request=",request.data);
+//         setWeather(request.data)
+//         // return request
+//     }
+//     fetchData();
+// },[])
 
-useEffect(()=>{
-  console.log("useEffect called");
-  async function fetchData(){
-      const options = {
-              method: 'GET',
-              url: 'https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/',
-              headers: {
-                  'x-rapidapi-host': 'forecast9.p.rapidapi.com',
-                  'x-rapidapi-key': '47cd745e3amshab32e2942dd9b87p1e6fa4jsn14c37e1f98f1'
-              }
-          };
-      const request=await axios.request(options)
-      console.log("Forecast Request=",request.data);
-      setForecast(request.data)
-      // return request
-  }
-  fetchData();
-},[])
+// useEffect(()=>{
+//   console.log("useEffect called");
+//   async function fetchData(){
+//       const options = {
+//               method: 'GET',
+//               url: 'https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/',
+//               headers: {
+//                   'x-rapidapi-host': 'forecast9.p.rapidapi.com',
+//                   'x-rapidapi-key': '92892ab54amshf55ba9722bf8547p1eedb8jsn24eaeb644984'
+//               }
+//           };
+//       const request=await axios.request(options)
+//       console.log("Forecast Request=",request.data);
+//       setForecast(request.data)
+//       // return request
+//   }
+//   fetchData();
+// },[])
+
+// useEffect(()=>{
+//   console.log("useEffect called");
+//   async function fetchData(){
+//       const options = {
+//               method: 'GET',
+//               url: 'https://air-quality.p.rapidapi.com/current/airquality',
+//               params:{lon: '-73.00597', lat: '40.71427'},
+//               headers: {
+//                   'x-rapidapi-host': 'air-quality.p.rapidapi.com',
+//                   'x-rapidapi-key': '92892ab54amshf55ba9722bf8547p1eedb8jsn24eaeb644984'
+//               }
+//           };
+//       const request=await axios.request(options)
+//       console.log("AirQuality Request=",request.data);
+//       setAirquality(request.data)
+//       // return request
+//   }
+//   fetchData();
+// },[])
+
+// useEffect(()=>{
+//   console.log("useEffect called");
+//   async function fetchData(){
+//       const options = {
+//               method: 'GET',
+//               url: 'https://meteostat.p.rapidapi.com/stations/nearby',
+//               params: {lat: '51.5085', lon: '-0.1257'},
+//               headers: {
+//                   'x-rapidapi-host': 'meteostat.p.rapidapi.com',
+//                   'x-rapidapi-key': '92892ab54amshf55ba9722bf8547p1eedb8jsn24eaeb644984'
+//               }
+//           };
+//       const request=await axios.request(options)
+//       console.log("Station Request=",request.data);
+//       setStation(request.data)
+//       // return request
+//   }
+//   fetchData();
+// },[])
 
   return (
     <Router>
@@ -71,10 +115,10 @@ useEffect(()=>{
             <Switch> 
               <Route exact path='/currentWeather' component={() => (<CurrentWeather weather={weather} />)}></Route>
               <Route exact path='/forecast' component={() => (<Forecast forecast={forecast} />)}></Route>
-              {/* <Route exact path='/cityWeather' component={CityWeather}></Route>
-              <Route exact path='/weatherStation' component={WeatherStation}></Route>
-              <Route exact path='/tidalLength' component={TidalLength}></Route>
-              <Route exact path='/airQuality' component={AirQuality}></Route> */}
+              {/* <Route exact path='/cityWeather' component={CityWeather}></Route>*/}
+              <Route exact path='/weatherStation' component={() => (<WeatherStation station={station} />)}></Route>
+              {/* <Route exact path='/tidalLength' component={TidalLength}></Route> */}
+              <Route exact path='/airQuality' component={() => (<AirQuality airquality={airquality} />)}></Route> 
             </Switch> 
       </div>
     </Router>
--- src/components/WeatherStation/WeatherStation.js
+++ src/components/WeatherStation/WeatherStation.js
@@ -1,80 +1,23 @@
-import React,{useState,useEffect} from 'react'
-import axios from "axios";
+import React from 'react'
+import "./WeatherStation.css"
 
-const station = () => {
-    const [station,setStation]=useState([])
+const station = ({station}) => {
+    const s=station
+    console.log("Station =",s);
 
-    // const options = {
-    // method: 'GET',
-    // url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-    // params: {
-    //     q: 'London,uk',
-    //     lat: '0',
-    //     lon: '0',
-    //     // callback: 'test',
-    //     id: '2172797',
-    //     lang: 'null',
-    //     units: 'imperial',
-    //     mode: 'json'
-    // },
-    // headers: {
-    //     'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-    //     'x-rapidapi-key': '1c4d378973mshc5b9af24f6084b6p1be392jsn5387d813d986'
-    // }
-    // };
-
-    // useEffect((options)=>{
-    //     axios.request(options)
-    //         .then(response => {
-    //             let vari=response.data
-    //             console.log(vari["coord"].lat);
-    //             setstation(vari);
-    //         }).catch(function (error) {
-    //             console.error(error);
-    //     });
-    // },[])
-
-    useEffect(()=>{
-        console.log("useEffect called");
-        async function fetchData(){
-            const options = {
-                    method: 'GET',
-                    url: 'https://meteostat.p.rapidapi.com/stations/nearby',
-                    headers: {
-                        'x-rapidapi-host': 'meteostat.p.rapidapi.com',
-                        'x-rapidapi-key': '47cd745e3amshab32e2942dd9b87p1e6fa4jsn14c37e1f98f1'
-                    }
-                };
-            const request=await axios.request(options)
-            console.log("Request=",request);
-            setStation(request.data)
-            // return request
-        }
-        fetchData();
-    },[])
-
-    // let a=axios.request(options)
-    //     .then(response => {
-    //         return response.data
-    //     }).catch(function (error) {
-    //         console.error(error);
-    // });
-
-    // console.log("A=",a);
-
-    // setstation(a);
-    
-    console.log("Weather=",setstation);
-
+    let a=Array(s.data)
+    console.log("A=",a[0][0].name.en);
+    var r=a[0].map((item)=>{
+        return (
+            <div className="bigDiv">
+                <p>Name of Station: {(item).name.en}</p>
+                <p>Distance of Station: {(item).distance}</p>
+            </div>
+        )
+    })
     return (
         <div>
-            let a=Array(station.data)
-            {a.fill().map((item)=>{
-                <div>
-                    <p>Name of Station: {item.name.en}</p>
-                    <p>Distance of Station: {item.distance}</p>
-                </div>
-            })}
+            {r}
         </div>
     )
 
